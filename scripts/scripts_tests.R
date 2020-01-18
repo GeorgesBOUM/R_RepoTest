397f1b7e975862b7ae3cc7747aa3f09121aa1a25
GeorgesBOUM <boumgeorges@protonmail.com>
2020-01-18 16:36:27 +0100

    adding a third line  to the script

--- scripts/scripts_tests.R
+++ scripts/scripts_tests.R
@@ -1,3 +1,4 @@
 test_matrix <- matrix(c(1,2,3,4,5,6,7,8), nrow = 4, ncol = 2)
 print("this is the first line in R studio after git setup")
 print("this is the second line in R studio after git setup")
+print("let's add a third line")
