6b2cc0178a0a308a395ce0bf87f5142baf4a59b9
GeorgesBOUM <boumgeorges@protonmail.com>
2020-01-18 16:08:38 +0100

    my first R script on github

---
+++ scripts/scripts_tests.R
@@ -0,0 +1,3 @@
+test_matrix <- matrix(c(1,2,3,4,5,6,7,8), nrow = 4, ncol = 2)
+print("this is the first line in R studio after git setup")
+print("this is the second line in R studio after git setup")
